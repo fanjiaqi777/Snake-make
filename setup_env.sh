43fbb9d60f99cb6a39b17c6d0a3b9bee1989fd44
Stan <92315317+fanjiaqi777@users.noreply.github.com>
2024-10-23 23:05:36 +0200

    Add files via upload
    RNA-seq的几个ref路径

---
+++ setup_env.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 设置工作路径
+export workdir=/public1/home/sch10755/02.data/Fan-data/work
+export refdir=$workdir/ref
+export datadir=$workdir/data
+export scriptdir=$workdir/scripts
+
+export REF_INDEX=$refdir/Prunus_persica.Prunus_persica_NCBIv2.dna.toplevel
+export GFF=$refdir/Prunus_persica.Prunus_persica_NCBIv2.51.gff3
+export GTF=$refdir/Prunus_persica.Prunus_persica_NCBIv2.51.gtf
+export GENE_BED=$refdir/gene.bed
+export GENE_LENGTH=$refdir/gene_length.txt
+
